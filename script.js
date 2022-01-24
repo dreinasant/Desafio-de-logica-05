943403a04d84f5a4a6da2f2e04fab35f60e70737
dreinasant <ao.andreina@gmail.com>
2022-01-24 20:12:06 -0300

    Desafio array

---
+++ script.js
@@ -0,0 +1,11 @@
+/*let colegas = ["Andrêina", "Pamela", "Barbara", "Larissa", "Ghislain"]
+console.log(colegas)
+
+colegas.unshift("Luan")
+
+colegas.splice(5)
+
+colegas.push("Tati", "Arthur")
+
+colegas.shift(0)*/
+
